5f91abbec4ec0971b10f32f4aa71417bdec08601
deshengliv1 <lideshengv@gmail.com>
2021-03-21 12:22:09 +0800

    投注记录

---
+++ src/components/betRecord/index.vue
@@ -0,0 +1,222 @@
+<template>
+  <div class="betRecord-screen normal-screen">
+    <van-row type="flex" justify="space-between" align="center" class="my-topvr title-bar">
+      <div class="return">
+        <img src="../../assets/首页/return.png"/>
+      </div>
+      <div class="title">投注记录</div>
+      <div class="right-action">
+        <img src="../../assets/首页/新建文件夹/icon_msg_center@2x.png" class="msg-icon" />
+      </div>
+    </van-row>
+    <van-tabs v-model="curTab" sticky animated
+              @change="changeTab"
+              line-width="40px"
+              line-height="2px"
+              color="#e5c7a6"
+              title-active-color="#be987f"
+              title-inactive-color="#A4A9B4"
+              class="normal-tab">
+      <van-tab v-for="item in gameTypes"
+               :title="item.gameName"
+               :key="item.gameKey"
+               :name="item.gameKey">
+        <div class="normal-bet-list">
+            <div class="bet-list-query">
+              <select @change="changeBetType">
+                <option>全部类型</option>
+              </select>
+              <select @change="changeBetTime">
+                <option>今天</option>
+              </select>
+            </div>
+            <div class="record-div" :class="'record-div-'+item.gameKey">
+              <van-row type="flex" justify="space-between" align="center" class="record-count">
+                <span>笔数:{{count.num}}</span>
+                <span>流水:{{count.money}}</span>
+                <span>输赢:{{count.win}}</span>
+              </van-row>
+              <van-list v-model="loading" :finished="finished" :finished-text="finishedText" @load="loadPage">
+                <template v-for="record in data">
+                  <div class="record-item-div">
+                    <van-row type="flex" justify="space-between" align="center" class="record-item-top">
+                      <div class="record-item-day">{{record.betDay}}</div>
+                      <div>
+                        <span>笔数：{{record.num}}</span>
+                        <span>流水：{{record.money}}</span>
+                        <span>输赢：{{record.win}}</span>
+                      </div>
+                    </van-row>
+                    <div class="record-item-swap">
+                      <van-row type="flex" justify="space-between" align="center" class="record-item-title">
+                        <span class="record-item-day">{{record.betType}}</span>
+                        <span class="record-bet-state" :class="'bet-state-'+record.state">投注成功</span>
+                      </van-row>
+                      <van-row type="flex" justify="space-between" align="center" class="record-item-game">
+                        <span class="record-item-game-name" :style="gameLeftImg">{{record.gameName}}</span>
+                        <span class="record-item-game-time">比赛时间：{{record.gameTime}}</span>
+                      </van-row>
+                      <div class="record-item-gamevs">{{record.gameVsInfo}}</div>
+                      <van-row type="flex" justify="space-between" align="center" class="record-item-info">
+                        <span class="item-game-cate">{{record.betName}}</span>
+                        <span class="item-game-value">{{record.betValue}}</span>
+                      </van-row>
+                      <van-row type="flex" justify="space-between" align="center" class="record-item-info">
+                        <span class="item-game-betcate">{{record.betCate}}</span>
+                        <span class="item-game-value">{{record.betCateVal}}</span>
+                      </van-row>
+                      <van-row type="flex" justify="space-between" align="center" class="record-item-info">
+                        <span class="item-game-bet">投注金额：{{record.money}}</span>
+                        <span class="item-game-value">输/赢：{{record.win}}</span>
+                      </van-row>
+                      <van-row type="flex" justify="space-between" align="center" class="record-item-order">
+                        <span class="item-game-bet">订单号：{{record.orderId}}</span>
+                        <span class="item-game-value">投注时间：{{record.orderTime}}</span>
+                      </van-row>
+                    </div>
+                  </div>
+                </template>
+              </van-list>
+            </div>
+        </div>
+      </van-tab>
+    </van-tabs>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return{
+        curTab:"zq",
+        pageSize:20,
+        data:[],
+        finishedText: "-已加载全部内容-",
+        page: 1, //页数
+        loading: false,
+        finished: false,
+        count:{num:1,money:10.00,win:-10.00},
+        curQryParam:{},
+        gameTypes:[
+          {gameName:"足球",gameKey:"zq"},
+          {gameName:"篮球",gameKey:"lq"},
+          {gameName:"排球",gameKey:"pq"},
+          {gameName:"网球",gameKey:"wq"},
+          {gameName:"乒乓球",gameKey:"ppq"},
+          {gameName:"冰球",gameKey:"bq"}
+        ],
+        gameLeftImg:{
+          backgroundImage: 'url(' + require('../../assets/首页/新建文件夹/icon_league@2x.png') + ')',
+        }
+      }
+    },
+    mounted() {
+
+    },
+    methods:{
+      changeTab(){
+        this.initPage();
+      },
+      initPage:function(){
+        this.page=1;
+        this.data=[];
+        this.loading=false;
+        this.finished=false;
+      },
+      loadPage:function(){
+        //this.$ajax.get('').then(res => {});
+        this.loading=false;
+        this.finished=true;
+        this.data=[
+          {state:"1",num:"1",money:"12.12",win:"-12.12",betType:"单式-金龙体育",
+            gameName:"波兰女士杯",gameTime:"2021-03-20 12:12:12",gameVsInfo:"sdfsdf vs  sdfdsf",
+            betName:"滚球 独赢",betValue:"输",betCate:"四道口附近v",betCateVal:"全场1-1",
+            money:10.22,win:-10.22,orderId:234233453454,orderTime:"02/23 12:12",betDay:"03月12日"
+          }
+        ];
+      },
+      changeBetType(){
+
+      },
+      changeBetTime(){
+
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .title-bar .right-action img{
+    position: initial;
+  }
+  .bet-list-query{
+    background: white;
+    padding: 12px 20px;
+    border-top: 1px solid #f5f3f3;
+    select{
+      border: 1px solid #f1f1f1;
+      border-radius: 18px;
+      padding: 5px 0px;
+      background: #f1f1f1;
+      width: 120px;
+      text-align: center;
+      text-align-last: center;
+    }
+  }
+  .normal-bet-list{
+    .record-count{
+      border-top: 1px solid #eaeaea;
+      padding: 8px;
+      line-height: 30px;
+    }
+    .record-item-div{
+      width: 95%;
+      margin: 0px auto;
+      .record-item-swap{
+        background: white;
+        border-radius: 8px;
+        box-sizing: border-box;
+        &>div{
+          padding:5px 10px;
+        }
+      }
+      .record-item-top{
+        border-top: 1px solid #e0dfdf;
+        padding: 8px 0px;
+        div:last-child{
+          color: #bdbdbd;
+          font-size: 12px;
+        }
+      }
+      .record-item-title{
+        padding-bottom: 10px;
+        border-bottom: 1px solid #f3f1f1;
+        padding-top: 10px;
+        font-weight: bold;
+        color: #4e4c4c;
+        line-height: 30px;
+      }
+      .record-item-order{
+        color: #a29e9e;
+        font-size:12px;
+      }
+      .record-item-game{
+        color: #a29e9e;
+        font-size:12px;
+        .record-item-game-name{
+          background-repeat: no-repeat;
+          padding-left: 15px;
+          background-size: 12px 11px;
+          background-position-y: 3px;
+        }
+      }
+      .record-item-gamevs:before{
+        content: "";
+        width: 4px;
+        height: 20px;
+        background: rgb(229, 199, 166);
+        display: inline-block;
+        vertical-align: middle;
+        margin-right: 3px;
+      }
+    }
+  }
+</style>
